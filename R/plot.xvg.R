678e4d66b0132d1d0e46c89aa4d428cc5b424efd
Daniel Osorio <daniel.osorio@correo.uis.edu.co>
2014-02-18 00:00:00 +0000

    version 0.1

---
+++ R/plot.xvg.R
@@ -0,0 +1,9 @@
+plot.xvg<-function(x, ...){
+  data<-read.xvg(x)
+  X<-readLines(x)
+  P1<-grep(pattern="title",x=X[1:length(X)])
+  plot(data[,1],data[,2],type="l",
+       main=(scan(x,skip=min(P1)-1,nmax=min(P1)+2,what="",quiet=TRUE)[3]),
+       xlab=(scan(x,skip=min(P1)-1,nmax=min(P1)+2,what="",quiet=TRUE)[7]),
+       ylab=(scan(x,skip=min(P1)-1,nmax=min(P1)+2,what="",quiet=TRUE)[11]))
+}
